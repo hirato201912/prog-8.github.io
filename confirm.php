5664b78f0adc1394d727111e778a8b1db04461c5
hirato201912 <58656231+hirato201912@users.noreply.github.com>
2020-01-16 23:10:53 +0900

    Create confirm.php

---
+++ confirm.php
@@ -0,0 +1,43 @@
+<?php require_once('data.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>投稿内容</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="order-wrapper">
+  <div class="main">
+    <div class="thanks-message">投稿ありがとうございました</div>
+    <div class="display-contact">
+
+      <div class="form-title">入力内容</div>
+
+    
+      <!-- nameを受け取りechoしましょう -->
+      <?php echo $_POST["name"] ?>
+      
+      
+      <!-- bodyを受け取りechoしましょう -->
+      
+    </div>
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+    
+  </div>
+</body>
+</html>
